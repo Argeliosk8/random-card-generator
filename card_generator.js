62e3e1a0673dd61a2ac1f367028acf0a8f334ff5
unknown <argelio.baca@gmail.com>
2023-06-12 10:33:29 -0600

    added an if evaluation to make the diamond, heart and their numbers go red when selected + added a form and function to allow users to modify the size of the card

--- card_generator.js
+++ card_generator.js
@@ -3,25 +3,45 @@ const cardClasses = ['spade', 'heart', 'diamond', 'club']
 const cardNumbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
 
-function generateRandomSymbol() {
+const firstElement = document.getElementById('firstSymbol')
+const secondElement = document.getElementById('secondSymbol')
+const cardNumber = document.getElementById('cardNumber')
+const sizeEditor = document.getElementById('sizeEditor')
+const heightInput = document.getElementById('heightInput')
+const widthInput = document.getElementById('widthInput')
+const card = document.getElementById('card')
+
+function generateRandomSymbol() {    
     const randomSymbolIndex = Math.floor(Math.random() * 3 + 1)
-    const firstElement = document.getElementById('firstSymbol')
-    const secondElement = document.getElementById('secondSymbol')
     firstElement.innerText = cardSymbols[randomSymbolIndex]
     secondElement.innerText = cardSymbols[randomSymbolIndex]
     firstElement.className = cardClasses[randomSymbolIndex]
     secondElement.className = cardClasses[randomSymbolIndex]
+    if(randomSymbolIndex == 1 || randomSymbolIndex == 2){
+        cardNumber.style.color = 'red'
+    } else {
+        cardNumber.style.color = 'black'
+    }
+    
 }
 
 function generateRandomNumber(){
     const randomNumberIndex = Math.floor(Math.random() * 12 + 1)
-    const cardNumber = document.getElementById('cardNumber')
     cardNumber.innerText = cardNumbers[randomNumberIndex]
 }
 
 function renderRandomCard () {
     generateRandomSymbol()
     generateRandomNumber()
+}
+
+function changeCardSize(){
+    if(parseInt(heightInput.value) > 0 && parseInt(widthInput.value) > 0){
+        card.style.height = `${heightInput.value}px`
+        card.style.width = `${widthInput.value}px`
+    } else {
+        alert('You need to insert numbers greater than 0')
+    }
 }
 
 setInterval(renderRandomCard, 3000)
